54b3d003091b95e47e714cc5df2b9d8100da7c7c
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-03-03 09:26:10 -0500

    app finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,41 @@ import "./styles.css"
 
 export default function Game() {
 
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
 
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+
+  
   /* We can use states in the parent 
   if the components children will use the info in alls */
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove]
 
   function handlePlay(nextSquares) {
 
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  }
+  )
 
 
   return (
@@ -24,7 +47,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
