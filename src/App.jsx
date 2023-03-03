d2771ebd5b23bcbb9be6e012faa35a5a4c9b8051
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-03-02 22:32:53 -0500

    using the parent to controll the states of the childrens

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,23 @@
  * In React, a component is a piece of reusable code that represents a part of a user interface. 
  * Components are used to render, manage, and update the UI elements in your application
  */
+import { useState } from "react";
 import Square from "./components/Square"
 import "./styles.css"
 
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  const onSquareClick = (i) => {
+
+    const nextSquares = squares.slice();
+    nextSquares[i] = "x"
+    setSquares(nextSquares);
+}
+
+
+  const data = "hi i am a prop"
   return (
     <> 
       {/* React components need to return a single JSX 
@@ -17,19 +30,19 @@ export default function Board() {
       <div className="board-row">
               {/* We can send data from the parent to the
               child component by props(propertys) */}
-        < Square value="1"/>
-        < Square value="2"/>
-        < Square value="3" />
+        < Square prop={data} value={squares[1]} onSquareClick={() => onSquareClick(1)} />
+        < Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
+        < Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
       </div>
       <div className="board-row">
-        < Square value="4"/>
-        < Square value="5" />
-        < Square  value="6"/>
+        < Square  value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+        < Square  value={squares[5]} onSquareClick={() => onSquareClick(5)} />
+        < Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
       </div>
       <div className="board-row">
-        < Square value="7" />
-        < Square value="8"/>
-        < Square value="9"/>
+        < Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+        < Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
+        < Square value={squares[9]} onSquareClick={() => onSquareClick(9)}  />
       </div>
     </>
   )
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,32 @@
-export default function Square({ value }) {
+import { useState } from "react"
+
+export default function Square({ prop, value, onSquareClick  }) {
                     /* To read a data that was send by props
                     we have to use the destructuring syntax
                      */
+  
+  console.log(value)
+  
+  /* What is a state?
+
+  The components some time need to change what react
+  is showing in the ui as a result of a interantion
+
+  So the states in less words
+  are data that can change in the time like
+  the memory of a component
+  */
+  
+  /* We can do a component interactive using events handlers */
+
+
   return (
-    <button className="square">{value}</button>
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
                         /* To use javascript values in the jsx
                         the javascript code has to be inside
                         the curly brackets
